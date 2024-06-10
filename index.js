bb0388f4ceebebb18a8e940565a408a728f3b8a9
pooja <pooja@gamil.com>
2024-06-10 13:21:40 +0530

    initioal setup

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const cors = require("cors")
+require("dotenv").config()
+const mongoose = require("mongoose")
+const { logger } = require("./middlewares/logger")
+mongoose.connect(process.env.MONGO_URL)
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(logger)
+
+app.use("/api/auth", require("./routes/auth.routes"))
+app.use("/api/account", require("./routes/account.routes"))
+
+app.use("*", (req, res) => {
+    res.status(404).json({ message: "Resource Not Found" })
+})
+app.use("*", (req, res) => {
+    res.status(500).json({ message: "server Error",error:err.message })
+})
+mongoose.connection.once("open", () => {
+    console.log("MONGO CONNECTED")
+    app.listen(process.env.PORT, console.log("SERVER RUNNING"))
+})
